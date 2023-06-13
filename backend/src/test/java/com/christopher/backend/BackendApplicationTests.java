de6aa5eeb6dfb15d5ddbf244e6ef6417edb2f9a4
Christopher Vennemann <christopher.vennemann@klosebrothers.de>
2023-06-13 12:57:16 +0200

    backend: implement first test get request returns status 200

--- backend/src/test/java/com/christopher/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/christopher/backend/BackendApplicationTests.java
@@ -1,13 +1,33 @@
 package com.christopher.backend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class BackendApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
+		assertNotNull(mockMvc);
 	}
 
+	@Test
+	public void shouldReturnStatusOk200() throws Exception {
+		RequestBuilder request = MockMvcRequestBuilders.get("/items");
+
+		mockMvc.perform(request)
+				.andExpect(status().isOk());
+	}
 }
