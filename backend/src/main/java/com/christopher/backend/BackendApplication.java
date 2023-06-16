6b52e6bed1ee6da1c142f8279e588e5a472a943c
Christopher Vennemann <christopher.vennemann@klosebrothers.de>
2023-06-16 10:30:56 +0200

    added data.sql file to init dummy data from; removed hacky data entry from BackendApplication.java; ItemControllerTest now works fine!

--- backend/src/main/java/com/christopher/backend/BackendApplication.java
+++ backend/src/main/java/com/christopher/backend/BackendApplication.java
@@ -10,23 +10,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.Arrays;
 
 @SpringBootApplication
-public class BackendApplication implements CommandLineRunner {
-
-	@Autowired
-	ItemRepository itemRepository;
+public class BackendApplication /*implements CommandLineRunner*/ {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Item[] items = new Item[] {
-				new Item("item 1", 1L),
-				new Item("item 2", 2L),
-				new Item("item 3 long item", 3L),
-				new Item("item 4", 4L)
-		};
-		itemRepository.saveAll(Arrays.asList(items));
 	}
 }
