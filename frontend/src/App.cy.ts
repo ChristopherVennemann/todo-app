6cc9ea97ed185121c08252401257c208b5606e10
Christopher Vennemann <christopher.vennemann@klosebrothers.de>
2023-06-16 12:53:42 +0200

    added first frontend test with cypress

---
+++ frontend/src/App.cy.ts
@@ -0,0 +1,13 @@
+import App from './App.vue'
+
+describe('<App />', () => {
+  it('renders', () => {
+    // see: https://on.cypress.io/mounting-vue
+    cy.mount(App)
+  })
+
+  it('has four items', () => {
+    cy.mount(App)
+    cy.get('[data-cy=item]').should('have.length', 4)
+  })
+})
