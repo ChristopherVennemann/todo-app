234490738e9815a47ba201f253885d23ec8c80c0
Christopher Vennemann <christopher.vennemann@klosebrothers.de>
2023-06-19 20:12:13 +0200

    added cypress test for adding new item, removed old test that tested for fixed number of items

--- frontend/src/components/__tests__/App.cy.ts
+++ frontend/src/components/__tests__/App.cy.ts
@@ -7,8 +7,26 @@ describe('<App />', () => {
     cy.mount(App)
   })
 
-  it('has four items', () => {
+  it('has no initial items', () => {
     cy.mount(App)
-    cy.get('[data-cy=item]').should('have.length', 4)
+    cy.get('[data-cy=item]').should('have.length', 0)
+  })
+
+  it('adds new item', () => {
+    cy.mount(App)
+    cy.intercept('POST', '/items', {
+      statusCode: 201,
+      body: {
+        message: 'test'
+      },
+    })
+
+    cy.get('input').type('testing')
+    cy.get('button').click()
+
+    cy.get('[data-cy=item]').should(($items) => {
+      expect($items).to.have.length(1)
+      expect($items.eq(0)).to.contain('test')
+    })
   })
 })
