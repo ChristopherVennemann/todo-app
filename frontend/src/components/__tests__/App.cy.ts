6766d6386f590a7b02fcc1bdc73bf10f86081a51
Christopher Vennemann <christopher.vennemann@klosebrothers.de>
2023-06-20 16:13:50 +0200

    added more detailed frontend tests

--- frontend/src/components/__tests__/App.cy.ts
+++ frontend/src/components/__tests__/App.cy.ts
@@ -1,9 +1,9 @@
 // @ts-ignore
 import App from "../../App.vue";
+import Item from "@/types/Item";
 
 describe('<App />', () => {
   it('renders', () => {
-    // see: https://on.cypress.io/mounting-vue
     cy.mount(App)
   })
 
@@ -12,21 +12,44 @@ describe('<App />', () => {
     cy.get('[data-cy=item]').should('have.length', 0)
   })
 
-  it('adds new item', () => {
-    cy.mount(App)
-    cy.intercept('POST', '/items', {
-      statusCode: 201,
-      body: {
-        message: 'test'
-      },
-    })
+  it('serializes input value to post request body', () => {
+    const expectedMessage: String = 'item 1'
 
-    cy.get('input').type('testing')
+    cy.intercept('POST', '/items', {}).as('postItem')
+
+    cy.mount(App)
+    cy.get('input').type(expectedMessage)
+    cy.get('button').click()
+
+    cy.wait('@postItem').should(({ request, response }) => {
+      expect(request.body.message).to.equal(expectedMessage)
+    })
+  })
+
+  it('appends post response body to list', () => {
+    const expectedItem: Item = { message: 'test', id: 1 }
+
+    cy.intercept('POST', '/items', { statusCode: 201, body: expectedItem })
+
+    cy.mount(App)
     cy.get('button').click()
 
     cy.get('[data-cy=item]').should(($items) => {
       expect($items).to.have.length(1)
-      expect($items.eq(0)).to.contain('test')
+      expect($items.eq(0)).to.contain(expectedItem.message)
     })
+  })
+
+  it('clears input field on successful creation', () => {
+    const expectedEmptyString: String = ''
+
+    const validItem: Item = { message: 'foo', id: 1 }
+    cy.intercept('POST', '/items', { statusCode: 201, body: validItem })
+
+    cy.mount(App)
+    cy.get('input').type('bar')
+    cy.get('button').click()
+
+    cy.get('input').should('have.value', expectedEmptyString)
   })
 })
