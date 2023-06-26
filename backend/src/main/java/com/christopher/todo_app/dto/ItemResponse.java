3669a8c97f18a4dc47e1155daaa10e567a9f924e
Christopher Vennemann <christopher.vennemann@klosebrothers.de>
2023-06-26 15:28:49 +0200

    review mit ingolf und ewgenij

--- backend/src/main/java/com/christopher/todo_app/dto/ItemResponse.java
+++ backend/src/main/java/com/christopher/todo_app/dto/ItemResponse.java
@@ -5,28 +5,9 @@ import com.christopher.todo_app.entity.Item;
 import jakarta.annotation.Nonnull;
 import jakarta.validation.constraints.NotBlank;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ItemResponse {
-
-    public static ItemResponse of(Item item) {
-        ItemResponse itemResponse = new ItemResponse();
-        itemResponse.setMessage(item.getMessage());
-        itemResponse.setId(item.getId());
-        return itemResponse;
-    }
-
-    public static List<ItemResponse> of(List<Item> items) {
-        List<ItemResponse> itemResponses = new ArrayList<>();
-        for (Item item : items) {
-            ItemResponse itemResponse = new ItemResponse();
-            itemResponse.setMessage(item.getMessage());
-            itemResponse.setId(item.getId());
-            itemResponses.add(itemResponse);
-        }
-        return itemResponses;
-    }
 
     private Long id;
 
@@ -34,27 +15,37 @@ public class ItemResponse {
     @NotBlank(message = "message must not be empty")
     private String message;
 
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public ItemResponse(String message, Long id) {
         this.message = message;
         this.id = id;
     }
 
     public ItemResponse() {
+    }
+
+    public static ItemResponse of(Item item) {
+        return new ItemResponse(item.getMessage(), item.getId());
+    }
+
+    public static List<ItemResponse> of(List<Item> items) {
+        return items.stream()
+            .map(item -> new ItemResponse(item.getMessage(), item.getId()))
+            .toList();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(final String message) {
+        this.message = message;
     }
 }
